99b047eef647821c7c0dceabef2c1ace27c60526
SigneStenlev <wbj316@alumni.ku.dk>
2024-03-18 18:04:38 +0100

    Edgeworth box

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -17,6 +17,8 @@ def square(x):
 
 #Exchange economy from Project
 
+import numpy as np
+import matplotlib.pyplot as plt
 from types import SimpleNamespace
 
 class ExchangeEconomyClass:
@@ -30,8 +32,8 @@ class ExchangeEconomyClass:
         par.beta = 2/3
 
         # b. endowments
-        par.w1A >= 0.8
-        par.w2A >= 0.3
+        par.w1A = 0.8
+        par.w2A = 0.3
 
         # Total endowments
         par.w1B = 1 - par.w1A
@@ -47,11 +49,15 @@ class ExchangeEconomyClass:
 
     def demand_A(self,p1):
         par = self.par
-        return par.alpha*((p1*par.w1A+1*par.w2A)/(p1))
+        x1A = par.alpha * ((p1 * par.w1A + 1 * par.w2A) / (p1))
+        x2A = (1 - par.alpha) * ((p1 * par.w1A + 1 * par.w2A) / (p1))
+        return x1A, x2A
 
     def demand_B(self,p1):
         par = self.par
-        return par.beta*((p1*par.w1B+1*par.w2B)/(p1))
+        x1B = par.beta * ((p1 * par.w1B + 1 * par.w2B) / (p1))
+        x2B = (1 - par.beta) * ((p1 * par.w1B + 1 * par.w2B) / (p1))
+        return x1B, x2B
 
     def check_market_clearing(self,p1):
 
@@ -64,5 +70,43 @@ class ExchangeEconomyClass:
         eps2 = x2A-par.w2A + x2B-(1-par.w2A)
 
         return eps1,eps2
+    
+    def plot_edgeworth_box(self, N):
+        combinations_A = []
+        combinations_B = []
+
+        # Generate N evenly spaced values between 0 and 1
+        x_values = np.linspace(0, 1, 75)
+
+        for x1A in x_values:
+            for x2A in x_values:
+                # Check conditions for allocation (x1A, x2A) for individual A
+                if self.utility_A(x1A, x2A) > self.utility_A(self.par.w1A, self.par.w2A):
+                    # Calculate corresponding x1B and x2B for individual B
+                    x1B = 1 - x1A
+                    x2B = 1 - x2A
+                    # Check conditions for allocation (x1B, x2B) for individual B
+                    if self.utility_B(x1B, x2B) > self.utility_B(self.par.w1B, self.par.w2B):
+                        combinations_A.append([x1A, x2A])
+                        combinations_B.append([x1B, x2B])
+
+        combinations_A = np.array(combinations_A)
+        combinations_B = np.array(combinations_B)
+
+        plt.plot(combinations_A[:, 0], combinations_A[:, 1], 'o', label='Individual A', linewidth = 0)
+        #plt.plot(combinations_B[:, 0], combinations_B[:, 1], 'o', label='Individual B', linewidth = 0)
+        plt.plot(self.par.w1A, self.par.w2A, 'ro', label='Endowment A')
+        #plt.plot(self.par.w1B, self.par.w2B, 'bo', label='Endowment B')
+
+        plt.xlabel('x_1^a')
+        plt.ylabel('x_2^a')
+        plt.title('Edgeworth Box')
+        plt.legend()
+        plt.show()
+
+
+
+
+
 
 
